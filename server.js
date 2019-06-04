047aedc951119a55db3d60f40a654924a290382a
Rawl harris <rawlharris@yahoo.com>
2019-06-04 12:02:55 -0400

    settingPassportAuth

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const passport = require('passport');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// need these lines for passport Auth
+app.use(passport.initialize());
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
